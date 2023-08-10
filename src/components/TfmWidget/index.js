beca1fbeeb2d59c2825e2c96df9a8bc422c6eee5
Rahman <abdulrahman6093@outlook.com>
2023-08-10 10:55:25 +0530

    added TfmWidget

---
+++ src/components/TfmWidget/index.js
@@ -0,0 +1,11 @@
+import { IBCWidget } from '@0xtfm/widget-ibc';
+import '@0xtfm/widget-ibc/dist/styles.css';
+import React from 'react';
+
+const TfmWidget = () => {
+    return (
+        <IBCWidget />
+    );
+};
+
+export default TfmWidget;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './index.css';
+import TfmWidget from '../../components/TfmWidget';
 
 const Home = () => {
     return (
         <div className="home">
             Home
+            <TfmWidget/>
         </div>
     );
 };
